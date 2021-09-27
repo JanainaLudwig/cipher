a9f0de1e4c7e1dea90189d5b17973b1bddae9188
JanainaLudwig <janainaludwig@gmail.com>
2021-09-27 18:40:40 -0300

    version 1

--- cesar/main.go
+++ cesar/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"log"
+	"unicode"
 )
 
 func main() {
@@ -24,12 +25,12 @@ func main() {
 		Key: *chave,
 	}
 
-	c.rotateChar('a')
-	c.rotateChar('A')
-	c.rotateChar('0')
-	c.rotateChar('z')
-	c.rotateChar('Z')
-	c.rotateChar('9')
+	text := "texto de teste 1"
+
+	crypt := c.Crypt(text)
+	log.Println(crypt)
+	log.Println(c.Decrypt(crypt))
+
 }
 
 
@@ -37,33 +38,57 @@ type Cesar struct {
 	Key int
 }
 
-func (c *Cesar) rotateChar(char uint8) string {
+func (c *Cesar) Crypt(text string) string {
+	return c.convert(text, c.Key)
+}
+
+func (c *Cesar) Decrypt(text string) string {
+	return c.convert(text, c.Key * -1)
+}
+
+func (c *Cesar) convert(text string, key int) string {
+	var converted string
+	for _, char := range text {
+		if unicode.IsNumber(char) || unicode.IsLetter(char) {
+			converted += c.rotateChar(uint8(char), key)
+		} else {
+			converted += string(char)
+		}
+	}
+
+	return converted
+}
+
+
+func (c *Cesar) rotateChar(char uint8, key int) string {
 	var rotated int
 
-	if c.Key > 0 {
+	if key > 0 {
 		switch char {
 		case 122: // z
-			rotated = 96 + c.Key // a - 1
+
+
+			rotated = 96 + key // a - 1
 		case 90: // Z
-			rotated = 64 + c.Key // A - 1
+			rotated = 64 + key // A - 1
 		case 57: // 9
-			rotated = 47 + c.Key // 0 - 1
+			rotated = 47 + key // 0 - 1
 		default:
-			rotated = int(char) + c.Key
+			rotated = int(char) + key
 		}
 	} else {
 		switch char {
 		case 97: // - a
-			rotated = 123 + c.Key // a -
+			rotated = 123 + key // a -
 		case 65: // - A-
-			rotated = 91  + c.Key // A -
+			rotated = 91  + key // A -
 		case 48: // - 0-
-			rotated = 58  + c.Key // 0 -
+			rotated = 58  + key // 0 -
 		default:
-			rotated = int(char) + c.Key
+			rotated = int(char) + key
 		}
 	}
 
-	//log.Println(string(char), char, "=>", rotated, rune(rotated), string(rune(rotated)))
+	log.Println(string(char), char, "=>", rotated, rune(rotated), string(rune(rotated)))
 	return string(rune(rotated))
 }
